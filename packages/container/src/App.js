8ba9d8f3c243a1006815c66c14eb3e21e4d22af8
PriyaAugustin <dev.augustin00@gmail.com>
2022-08-04 08:18:48 -0500

    added generate classname features

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,17 +1,24 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/styles";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <h1>Hi there!!!!!!!!!!!!!!!</h1>
-        <hr />
-        <Header />
-        <MarketingApp />
-      </div>
+      <SstylesProvider generateClassName={generateClassName}>
+        <div>
+          <h1>Hi there!!!!!!!!!!!!!!!</h1>
+          <hr />
+          <Header />
+          <MarketingApp />
+        </div>
+      </SstylesProvider>
     </BrowserRouter>
   );
 };
